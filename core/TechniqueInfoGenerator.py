1d0882f51f556e4e6b0370ac422ad571566f0b95
Arpan Sarkar <asarkar@vectra.ai>
2024-03-09 12:45:37 -0800

    resource link display fixed

--- core/TechniqueInfoGenerator.py
+++ core/TechniqueInfoGenerator.py
@@ -1,4 +1,6 @@
-'''Generates Technique information offcanvas on the attack page'''
+'''
+Description : Generates Technique information from the MasterRecord.yml and displays it in the offcanvas on the attack page. The offcanvas is triggered by the About Technique button
+'''
 import yaml
 from dash import dcc, html
 
@@ -10,21 +12,24 @@ def TechniqueRecordInfo(t_id):
 
     technique_info_div = []
 
+    # display technique name
     technique_name = techniques_info[t_id]['Name']
     technique_info_div.append(html.H1(technique_name))
     technique_info_div.append(html.Br())
-
+    
+    # display attack surface info 
     t_attack_surface = techniques_info[t_id]['AttackSurface']
     technique_info_div.append(html.H2("Attack Surface :", className="text-success"))
     technique_info_div.append(html.H4(t_attack_surface))
     technique_info_div.append(html.Br())
 
+    # display mitre info
     t_mitre_info = techniques_info[t_id]['References']['MITRE']
     technique_info_div.append(html.H2("MITRE Info :"))
     technique_info_div.append(html.Br())
 
     for mitre_technique_id in t_mitre_info:
-        # Technique ID
+        # display technique ID
         technique_info_div.append(html.H3("Technique ID :", className="text-success"))
         if mitre_technique_id:
             technique_info_div.append(html.H4(mitre_technique_id))
@@ -32,7 +37,7 @@ def TechniqueRecordInfo(t_id):
             technique_info_div.append(html.H4("N/A"))
         technique_info_div.append(html.Br())
 
-        # Technique Name
+        # display technique Name
         t_mitre_technique = t_mitre_info[mitre_technique_id]['Technique']
         technique_info_div.append(html.H3("Technique :", className="text-success"))
         if t_mitre_technique:
@@ -41,7 +46,7 @@ def TechniqueRecordInfo(t_id):
             technique_info_div.append(html.H4("N/A"))
         technique_info_div.append(html.Br())
 
-        # Sub-technique
+        # display sub-technique
         t_mitre_subtechnique = t_mitre_info[mitre_technique_id]['SubTechnique']
         technique_info_div.append(html.H3("Sub-Technique :", className="text-success"))
         if t_mitre_subtechnique:
@@ -50,22 +55,21 @@ def TechniqueRecordInfo(t_id):
             technique_info_div.append(html.H4("N/A"))
         technique_info_div.append(html.Br())
 
-        # Tactics
+        # display tactics
         t_mitre_tactics = t_mitre_info[mitre_technique_id]['Tactic']
         technique_info_div.append(html.H3("Tactics :", className="text-success"))
         technique_info_div.append(html.H4(", ".join(t_mitre_tactics)))
         technique_info_div.append(html.Br())
 
-    #Resources
+    # display resources linked
     technique_resources = techniques_info[t_id]['Resources']
     technique_info_div.append(html.H2("Resources :", className="text-success"))
     for resource in technique_resources:
-        technique_info_div.append(dcc.Link(href = resource, target = "_blank"))
+        technique_info_div.append(html.Li(dcc.Link(href = resource, target = "_blank")))
     
     technique_info_div.append(html.Br())
-    technique_info_div.append(html.Br())
 
-    #Notes
+    # display notes
     if techniques_info[t_id]['Notes']:
         technique_notes = techniques_info[t_id]['Notes']
         technique_info_div.append(html.H2("Notes :", className="text-success"))
