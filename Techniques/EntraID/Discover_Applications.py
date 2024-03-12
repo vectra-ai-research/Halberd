78d069065f84542785335ca57c6c267e5a6e208c
Arpan Sarkar <asarkar@vectra.ai>
2024-03-12 02:20:22 -0400

    app recon output fixed

--- Techniques/EntraID/Discover_Applications.py
+++ Techniques/EntraID/Discover_Applications.py
@@ -1,12 +1,23 @@
-#Discover Applications
+'''
+Module Name : Discover_Applications.py
+Description : recon applications available in the tenant
+'''
 from core.GraphFunctions import graph_get_request
 
 def TechniqueMain():
     endpoint_url = "https://graph.microsoft.com/v1.0/applications/"
 
-    tenant_applications = graph_get_request(url = endpoint_url)
-    
-    return tenant_applications
+    try:
+        response = graph_get_request(url = endpoint_url)
+        
+    except Exception as e:
+        return e
+
+    output_response = {}
+    for application in response:
+        output_response[application['id']] = application
+
+    return output_response
 
 
 def TechniqueInputSrc() -> list:
