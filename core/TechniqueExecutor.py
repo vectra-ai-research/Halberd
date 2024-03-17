c084c5a159392e487863f6d92bf03a2f63db0541
Arpan Sarkar <asarkar@vectra.ai>
2024-03-17 14:19:49 -0700

    enhanced output display

--- core/TechniqueExecutor.py
+++ core/TechniqueExecutor.py
@@ -26,19 +26,29 @@ def TechniqueInputs(t_id):
 # function to parse technique output and return standardized output
 def TechniqueOutput(t_id, technique_input, file_content = None):
 
+    # execute technique
     execution_path = techniques_info[t_id]['ExecutionPath']
 
     exec_module_path = re.findall(r'[^\/\.]+', execution_path)
     exec_module = importlib.import_module(f"Techniques.{exec_module_path[0]}.{exec_module_path[1]}")
     print(f"Executing {exec_module}")
     TechniqueMainFunction = getattr(exec_module, "TechniqueMain")
-
+    
     if file_content != None:
-        response = TechniqueMainFunction(*technique_input, file_content)
+        raw_response = TechniqueMainFunction(*technique_input, file_content)
     else:
-        response = TechniqueMainFunction(*technique_input)
+        raw_response = TechniqueMainFunction(*technique_input)
 
-    '''Automatically cleanup output'''
+    # parse output
+    try:
+        # use technique output function to parse output
+        TechniqueOutputFunction = getattr(exec_module, "TechniqueOutput")
+        response = TechniqueOutputFunction(raw_response)
+    except:
+        # if output function is not defined for technique, parse raw response
+        response = raw_response
+
+    '''Format parsed response based on response data type (dict / list / str)'''
     if type(response) == list:
         if response == []:
             return html.H4("No results returned", style ={"textAlign": "center", "padding": "5px"})
@@ -51,18 +61,37 @@ def TechniqueOutput(t_id, technique_input, file_content = None):
         return html.Div(output_div_elements)
 
     elif type(response) == dict:
+        # if the response is a null then return message
         if response == {}:
             return html.H4("No results returned", style ={"textAlign": "center", "padding": "5px"})
         
         output_div_elements = []
         for item in response:
-            output_div_elements.append(html.H3(item))
-            output_div_elements.append(
-                html.Div([
-                    html.Div(str(response[item])),
-                ], style={"overflowY": "auto", "border":"1px solid #ccc"})
-            )
-            output_div_elements.append(html.Br())
+            output_div_elements.append(html.H3(f"{item}"))
+
+            if type(response[item]) == dict:
+                sub_output_div_elements = []
+                for sub_item in response[item]:
+                    sub_output_div_elements.append(
+                        f"{sub_item} : {response[item][sub_item]}"
+                    )
+                    sub_output_div_elements.append(html.Br())
+                    sub_output_div_elements.append(html.Br())
+                
+                output_div_elements.append(
+                    html.Div([
+                        html.Div(sub_output_div_elements),
+                    ], style={"overflowY": "auto", "border":"1px solid #ccc"})
+                )
+                output_div_elements.append(html.Br())
+
+            else:
+                output_div_elements.append(
+                    html.Div([
+                        html.Div(str(response[item])),
+                    ], style={"overflowY": "auto", "border":"1px solid #ccc"})
+                )
+                output_div_elements.append(html.Br())
 
         return html.Div(output_div_elements)
 
