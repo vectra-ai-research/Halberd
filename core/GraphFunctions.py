6a7e43b8772db330dde415383496045ae8662fdd
Arpan Sarkar <asarkar@vectra.ai>
2024-03-29 14:52:44 -0700

    param support in get requests

--- core/GraphFunctions.py
+++ core/GraphFunctions.py
@@ -1,18 +1,19 @@
 import requests
 import json
+import re
 from core.EntraAuthFunctions import CreateHeader, FetchSelectedToken
 
 graph_base_url = "https://graph.microsoft.com/v1.0/"
 
 #Function to make GET request to Graph
-def graph_get_request(url,pagination=True):
+def graph_get_request(url, params = None, pagination = True):
 
     headers = CreateHeader(FetchSelectedToken())
     graph_results = []
         
     while url:
         try:
-            graph_result = requests.get(url=url, headers=headers).json()
+            graph_result = requests.get(url=url, headers=headers, params=params).json()
 
             if 'value' in graph_result.keys():
                 graph_results.extend(graph_result['value'])
@@ -80,3 +81,10 @@ def graph_put_request(url, data):
         print(e)
         print('PUT request failed!')
         return None
+
+def graph_check_guid(inp_string):
+    guid_regex = re.compile(r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$', re.I)
+    if bool(guid_regex.match(inp_string)):
+        return True
+    else:
+        return False
