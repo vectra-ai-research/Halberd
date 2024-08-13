61d812660e84f421bdcb506b009a1529ebd48024
Arpan Sarkar <asarkar@vectra.ai>
2024-08-12 23:09:18 -0700

    layout updated
    - page header added
    
    - side padding added

--- pages/attack.py
+++ pages/attack.py
@@ -1,13 +1,14 @@
 '''
 Page Navigation url : app/attack
 
-Page Description : Allows interaction with Halberd attack modules. Displays attack surfaces, tactics, techniques and corresponding technique config. The main content of the page is generated dynamically by core/TabContentGenerator.py (generates the content as per selected attack surface tab), core/TechniqueOptionsGenerator.py (generates the techniques available in each tactic of an attack surface) & core/TechniqueInfoGenerator.py (generates techniques information pane). The technique configuration form is generated dynamically by callback C004.
+Page Description : Allows interaction with Halberd attack modules. Displays attack surfaces, tactics, techniques and corresponding technique config. The main content of the page is generated dynamically by core.Functions.TabContentGenerator (generates the content as per selected attack surface tab), core.Functions.TechniqueOptionsGenerator (generates the techniques available in each tactic of an attack surface) & core.Functions.TechniqueInfoGenerator (generates techniques information pane). The technique configuration form is generated dynamically by callback C004.
 '''
 
 import dash_bootstrap_components as dbc
 from dash import html
-
+                   
 page_layout = html.Div([
+    html.H2("Attack Console", className="text-success mb-3"),
     dbc.Tabs(
         [
             dbc.Tab(label="EntraID", tab_id="tab-attack-EntraID", labelClassName="text-success"),
@@ -20,4 +21,4 @@ page_layout = html.Div([
         class_name="bg-dark"
     ),
     html.Div(id="tabs-content-div",className="bg-dark", style={"height": "90vh", "justify-content": "center", "align-items": "center"})
-], className="bg-dark", style={"height": "100vh", 'overflow': 'auto'})
+], className="bg-dark", style={"height": "100vh", 'overflow': 'auto', "padding-right": "20px", "padding-left": "20px"})
