42b27faf1ac083b30d65a3040c032441582cc4c4
Arpan Sarkar <openrecon@protonmail.com>
2024-11-12 01:33:58 -0800

    fix(core_playbook): Update playbook step param default value

--- core/playbook/playbook_step.py
+++ core/playbook/playbook_step.py
@@ -5,5 +5,5 @@ class PlaybookStep:
     """Defines a step in the playbook"""
     def __init__(self, module: str, params: Optional[List[Any]], wait: Optional[int]):
         self.module = module
-        self.params = params if params is not None else []
+        self.params = params if params is not None else {}
         self.wait = wait
