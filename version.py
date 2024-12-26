47893adb1a032df3d6d6eb768a15865fb0523a29
Arpan Sarkar <openrecon@protonmail.com>
2024-12-26 14:55:07 -0800

    feat(version): Add new version file

---
+++ version.py
@@ -0,0 +1,9 @@
+"""Halberd version and metadata information"""
+
+# Version number format is: MAJOR.MINOR.PATCH 
+__version__ = "2.0.0"
+
+# Additional metadata
+__author__ = "Arpan Sarkar (@openrec0n)"
+__description__ = "Multi-Cloud Attack Tool"
+__repository__ = "https://github.com/vectra-ai-research/Halberd"
