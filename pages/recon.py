2c2e45d7a96db530c01bd166af0a56df3ff765a6
Arpan Sarkar <openrecon@protonmail.com>
2024-12-26 15:31:30 -0800

    refactor(pages): Halberd 2.0.0
    Apply new Halberd UI styling

--- pages/recon.py
+++ pages/recon.py
@@ -5,19 +5,48 @@ Page Description : Recon page hosts various reconnaissance dashboards providing 
 
 from dash import html
 import dash_bootstrap_components as dbc
-from dash_iconify import DashIconify
 
-page_layout = html.Div([
-    html.H2(["Recon ", html.A(DashIconify(icon="mdi:help-circle-outline", width=18, height=18), href="https://github.com/vectra-ai-research/Halberd/wiki/UI-&-Navigation#recon-recon", target="_blank")], className="text-success mb-3"),
-    dbc.Tabs(
-        [
-            dbc.Tab(label="Roles", tab_id="tab-recon-roles", labelClassName="text-success"),
-            dbc.Tab(label="Users", tab_id="tab-recon-users", labelClassName="text-success"),
-            dbc.Tab(label="Entity Map", tab_id="tab-recon-entity-map", labelClassName="text-success")
-        ],
-        id="recon-target-tabs",
-        active_tab="tab-recon-roles",
-        class_name="bg-dark"
-    ),
-    html.Div(id="recon-content-div",className="bg-dark", style={"height": "90vh", "justify-content": "center", "align-items": "center"}),
-], className="bg-dark", style={"height": "100vh", "overflow": "auto", "padding-right": "20px", "padding-left": "20px"})
+page_layout = html.Div(
+    [
+        # Recon dashboard tabs
+        dbc.Tabs(
+            [
+                dbc.Tab(
+                    label="Roles", 
+                    tab_id="tab-recon-roles", 
+                    labelClassName="halberd-brand-heading text-danger"
+                ),
+                dbc.Tab(
+                    label="Users", 
+                    tab_id="tab-recon-users", 
+                    labelClassName="halberd-brand-heading text-danger"
+                ),
+                dbc.Tab(
+                    label="Entity Map", 
+                    tab_id="tab-recon-entity-map", 
+                    labelClassName="halberd-brand-heading text-danger"
+                )
+            ],
+            id="recon-target-tabs",
+            active_tab="tab-recon-roles",
+            class_name="bg-halberd-dark"
+        ),
+        # Div to display recon dashboards
+        html.Div(
+            id="recon-content-div",
+            className="bg-halberd-dark", 
+            style={
+                "height": "90vh", 
+                "justify-content": "center", 
+                "align-items": "center"
+            }
+        ),
+    ], 
+    className="bg-halberd-dark", 
+    style={
+        "height": "100vh", 
+        "overflow": "auto", 
+        "padding-right": "20px", 
+        "padding-left": "20px"
+    }
+)
